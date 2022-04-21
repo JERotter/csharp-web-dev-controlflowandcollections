ec6b17553ee4347fc7c51cf9662520c3f3f87bd1
JERotter <49504679+JERotter@users.noreply.github.com>
2022-04-21 14:09:11 -0400

    List practice

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace ListExercises
 {
@@ -6,7 +8,18 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+
+            List<int> numList = new List<int> {1,1,2,3,5,8,12,3,6};
+            int addArr = 0;
+            foreach(int num in numList)
+            {
+                if (num % 2 == 0)
+                {
+                    addArr += num;
+                    
+                }
+            }
+            Console.WriteLine(addArr);
         }
     }
 }
