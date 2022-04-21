a28fb7fcc4f039358bbf820003fc95aaa29792b9
JERotter <49504679+JERotter@users.noreply.github.com>
2022-04-21 13:51:25 -0400

    String practice

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,13 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string greenEggs = "I would not, could not, in a box. I would not, could not with a fox.I will not eat them in a house. I will not eat them with a mouse.";
+            string[] greenHam = greenEggs.Split(' ');
+            Console.WriteLine(string.Join(",", greenHam));
+            //foreach (string greenFood in greenHam)
+            //{
+            //    Console.WriteLine(greenFood);
+            //}
         }
     }
 }
